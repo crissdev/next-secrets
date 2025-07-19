7420ae2123dc9029de6d3c2df37319de06f80f5d
crissdev <criss.trifan@gmail.com>
2025-07-19 10:43:47 +0300

    Memoise fetch projects

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,12 +1,16 @@
 import { revalidateTag, unstable_cache } from 'next/cache';
+import { cache } from 'react';
 
 import { getProjects } from './services/projects.service';
 
 const FETCH_PROJECTS_TAG = 'projects';
 
-export const fetchProjects = unstable_cache(async () => await getProjects(), [], {
+// Cached functions
+const cachedGetProjects = cache(getProjects);
+
+export const fetchProjects = unstable_cache(async () => await cachedGetProjects(), [], {
   tags: [FETCH_PROJECTS_TAG],
-  revalidate: 300, // Revalidate every 5 minutes
+  revalidate: 5 * 60,
 });
 
 export function revalidateProjects() {
