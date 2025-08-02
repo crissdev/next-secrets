f01c19dfa9973006e6593b16b451282de6dd34ec
crissdev <criss.trifan@gmail.com>
2025-08-02 18:35:05 +0300

    Remove demo test file

--- tests/unit/demo.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-
-describe('Demo tests', () => {
-  test('should pass a simple test', () => {
-    const { getByRole } = render(<h1>Hello</h1>);
-    expect(getByRole('heading')).toHaveTextContent('Hello');
-  });
-});
