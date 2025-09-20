3c4a811b3542f2980c660ee2e82db8ccea991886
crissdev <criss.trifan@gmail.com>
2025-09-20 11:03:04 +0300

    Remove unused enum

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -23,14 +23,6 @@ export interface Environment {
   name: string;
 }
 
-export enum SECRET_TYPE {
-  API_KEY = 'API_KEY',
-  CONNECTION_STRING = 'CONNECTION_STRING',
-  PASSWORD = '',
-  TOKEN = 'TOKEN',
-  OTHER = 'OTHER',
-}
-
 // Update prisma/seed.ts if you change this
 export const DEFAULT_ENVIRONMENTS: Environment[] = [
   { id: 'bbfd5e06-d229-4df2-8816-0702352ee62d', name: 'Development' },
