396ad168a6bf998e84b2d73256dafd3e646cf1ab
crissdev <criss.trifan@gmail.com>
2025-09-20 16:14:20 +0300

    Move formatDate function to secrets-table component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,25 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-export function formatDate(date: Date | string) {
-  const now = new Date();
-  if (typeof date === 'string') date = new Date(date);
-  const diffMs = now.getTime() - date.getTime();
-  const diffSecs = Math.floor(diffMs / 1000);
-  const diffMins = Math.floor(diffSecs / 60);
-  const diffHours = Math.floor(diffMins / 60);
-  const diffDays = Math.floor(diffHours / 24);
-  const diffMonths = Math.floor(diffDays / 30);
-
-  if (diffMonths > 0) {
-    return `${diffMonths} ${diffMonths === 1 ? 'month' : 'months'} ago`;
-  } else if (diffDays > 0) {
-    return `${diffDays} ${diffDays === 1 ? 'day' : 'days'} ago`;
-  } else if (diffHours > 0) {
-    return `${diffHours} ${diffHours === 1 ? 'hour' : 'hours'} ago`;
-  } else if (diffMins > 0) {
-    return `${diffMins} ${diffMins === 1 ? 'minute' : 'minutes'} ago`;
-  } else {
-    return 'Just now';
-  }
-}
