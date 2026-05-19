5cb8e637afe0f7bdc89ada8c28715e3b7d5dd8f9
crissdev <criss.trifan@gmail.com>
2026-05-19 13:04:16 +0200

    Remove obsolete middleware file

--- middleware.ts
+++
@@ -1,5 +0,0 @@
-export { proxy as middleware } from '@/proxy';
-
-export const config = {
-  matcher: ['/((?!sign-in|sign-up|forgot-password|reset-password|api/auth|_next/static|_next/image|favicon\\.ico).*)'],
-};
